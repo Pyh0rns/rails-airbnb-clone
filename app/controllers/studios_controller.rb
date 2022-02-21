df571940530e18088944a6334d651c2389c48063
PierreYves Hoorens <pyhoorens@gmail.com>
2022-02-21 20:00:23 +0100

    studios crud finished

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,6 +7,37 @@ class StudiosController < ApplicationController
     @studio = Studio.find(params[:id])
   end
 
+  def new
+    @studio = Studio.new
+  end
+
+  def create
+    @user_id = 1 # to be changed with real user id
+    @studio = Studio.new(studio_params)
+    @studio.user_id = @user_id
+    if @studio.save
+      redirect_to studio_path(@studio)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @studio = Studio.find(params[:id])
+  end
+
+  def update
+    @studio = Studio.find(params[:id])
+    @studio.update(studio_params)
+    redirect_to studios_path
+  end
+
+  def destroy
+    @studio = Studio.find(params[:id])
+    @studio.destroy
+    redirect_to studios_path
+  end
+
   private
 
   def studio_params
