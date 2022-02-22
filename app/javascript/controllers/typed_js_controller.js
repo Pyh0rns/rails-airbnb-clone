8ee8385a264b74a89de905ccdfe5e8d9c2a20bd2
Felix Foiret <felix.foiret@outlook.com>
2022-02-22 11:53:53 +0100

    show view modify / carouselle add

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,14 +1,14 @@
-// app/javascript/controllers/typed_js_controller.js
-import { Controller } from "@hotwired/stimulus"
-// Don't forget to import the NPM package
-import Typed from "typed.js"
+// // app/javascript/controllers/typed_js_controller.js
+// import { Controller } from "@hotwired/stimulus"
+// // Don't forget to import the NPM package
+// import Typed from "typed.js"
 
-export default class extends Controller {
-  connect() {
-    new Typed(this.element, {
-      strings: ["Let's play music !", "Together..."],
-      typeSpeed: 120,
-      loop: true
-    });
-  }
-}
+// export default class extends Controller {
+//   connect() {
+//     new Typed(this.element, {
+//       strings: ["Let's play music !", "Together..."],
+//       typeSpeed: 120,
+//       loop: true
+//     });
+//   }
+// }
