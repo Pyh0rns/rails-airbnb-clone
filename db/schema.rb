6e925dfb4416924ef98eaabc7fb2de7fe255df17
PierreYves Hoorens <pyhoorens@gmail.com>
2022-02-22 11:26:48 +0100

    url pic studio et seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_140020) do
+ActiveRecord::Schema.define(version: 2022_02_22_093726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_140020) do
     t.date "available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo_url"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
